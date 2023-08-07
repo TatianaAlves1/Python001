2d577d4dccdd01d2d77b45a6868da1ea21dcbb01
TatianaAlves1 <tatipublisher@gmail.com>
2023-08-07 10:18:29 -0400

    aula for e revisão

--- aula05/exemplo1.py
+++ aula05/exemplo1.py
@@ -1,9 +1,37 @@
-#estrutura de dados sequencial/ordenada de valores
-lista = [5,6,'caneta',6.11,3,'oi',33]
-print("Lista completa:",lista)
-print(type(lista))
-print("1º item da lista:",lista[0])
-print("1º e 2º itens da lista",lista[0:3])
-print("1º e 2º itens da lista",lista[:3])
-print("último item da lista",lista[-1])
-print("Mostrando do 3º item ao último:",lista[2:])
+for i in range(9):
+    print(i,end=" ")
+print("\nValor do i:",i)
+for i in range(10,21):
+    print(i,end=" ")
+print("\nValor do i:",i)
+for i in range(10,100,5):
+    print(i,end=" ")
+print("\nValor do i:",i)
+for i in range(50,10,-1):
+    print(i,end=" ")
+print("\nValor do i:",i)
+v1 = 9
+v2 = 50
+for i in range(v1,v2):
+    print(i,end=" ")
+print("\nValor do i:",i)
+
+#leia 10 números e mostre msg informando que o número é ímpar e efetue a soma dos valores
+soma=0 
+for i in range(0,11):
+    num = float(input("Informe o número:"))
+    if num%2!=0:
+        print("Você digitou um número ímpar!")
+        soma +=num
+print("A soma dos números ímpares é ",soma)
+
+#leia 10 números inteiros e informe ao final quantos números pares foram digitados e soma total dos valores
+cont = 0
+soma = 0
+for i in range(1,11):
+    num = int(input("Informe um número inteiro:"))
+    if num%2==0:
+        cont +=1
+        soma +=num
+print(f"A quantidade de números pares digitados foi {cont} e o soma total ficou em {soma}")
+    
