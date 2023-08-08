c01356826e1b5bc03b68161a08c06e6e40c1513b
TatianaAlves1 <tatipublisher@gmail.com>
2023-08-08 11:56:29 -0400

    exemplo List Comprehensions

---
+++ aula07/exemplo3.py
@@ -0,0 +1,12 @@
+#operações sobre listas
+
+lista = [2,6,9,8,20,36,1,20,7,0,2]
+#criar uma lista com dobro de cada item
+dobro = [i*2 for i in lista]
+print(dobro)
+#lista com a metade dos valores dos itens
+metade  = [i/2 for i in lista]
+print(metade)
+#mostrar valores da lista que estão acima de 5
+lista2 = [i for i in lista if i>5]
+print(lista2)
