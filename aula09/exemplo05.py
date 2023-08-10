c3535a32f3d8f1f2e18f85c7490432f359beeeab
TatianaAlves1 <tatipublisher@gmail.com>
2023-08-10 11:49:21 -0400

    Tratamento de exceções e arquivos

--- aula09/exemplo05.py
+++ aula09/exemplo05.py
@@ -1,6 +1,15 @@
-try:
-    with open('aula09/arquivo-teste.txt','a',encoding='utf-8') as arquivo:
-        print("abriu...")
-        print(arquivo.read())
-except:
-    print("não rolou")
+while True:
+    try:
+        nome = input("Digite o nome:")
+        idade = int(input("Digite a idade:"))
+        with open('aula09/aula09.txt','a+',encoding='utf-8') as txt:
+            txt.write(f"Aluno: {nome:^15} idade: {idade:^6}\n")
+            try:
+                n = int(input("1 -  para visualizar\nQualquer tecla para encerrar:"))
+                if n == 1:
+                    txt.seek(0)
+                    print(txt.read())
+            except:
+                break
+    except:
+        print("Deu erro =(")
